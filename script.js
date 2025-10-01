20086a2a203db0394b0c68df8fc4f62d5ffba61c
kkxitlinn <145102969+kkxitlinn@users.noreply.github.com>
2025-10-01 21:16:55 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+// Flower data - add your friends' information here
+const flowerData = {
+    maddy: {
+        name: "Maddy <3 (Unhinged sexy lady)",
+        image: "images/maddy-flower-detail.png", // Replace with actual image path
+        description: "Dear my unhinged pookie, I am so grateful you walked into my life. I will forever be grateful knowing you are in my life and that I can message you about anything and you will not judge me. You mean the absolute world to me. Thank you for reading my book recommendations (hurry the fuck up and read gild). Thank you for kindly letting me in your house when I went to akl last time, you are so sweet, and kind. I cannot wait to see you and hangout with you again, you mean the world to me. You are so kind, so caring and drop dead gorgeous (ruin me). I love you so so much xx"
+    },
+    anne: {
+        name: "Anne <3 (Muscle Mummy)",
+        image: "images/anne-flower-detail.png",
+        description: "Dear muscle mummy, I am so lucky to be able to call you my book bestie. You are so kind and generous (also stunning) everyday, I am grateful knowing you are in my life. You mean so much to me and I just want you to know, if you ever need ANYTHING, I will always be here. A shoulder to cry on, someone to rant to, someones toes to lick, I am here. I love you so so much xx"
+    },
+    bris: {
+        name: "Bri <3 (Brittalicious)",
+        image: "images/bris-flower-detail.png",
+        description: "Dear the sweetest, most stunning gorgeous baby in the world. I am so fucking grateful for you being in my life it's actually genuinely not funny. You're the most gorgeous person inside and out. Thank you always for recommending the plated prisoner series. I am genuinely the luckiest person in the world to have you as a friend. Knowing I can rely on you for absolutely anything, youre the sweetest soul ever. Thank you for being in my life. I love you so fucking much (p.s. stop judging for the way i say midas you bully)"
+    },
+    christine: {
+        name: "Christine <3 (Beautiful lady)",
+        image: "images/christine-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    ell: {
+        name: "Ell <3 (Airpod Toes)",
+        image: "images/ell-flower-detail.png",
+        description: "Dear airpod toes... where the fuck do I start with you. Thank you for being amazing, caring and unhinged with me. Didn't realise you had a dark side when I first met you and now after spending 6+ months knowing you, girl you cray cray (in the best way). Thank you for just being fucking amazing. I love you to bits and I am so grateful you're in my life. I wouldn't have it any other way. You brighten my days so often and you know how to make someone feel better. Thank you for being you. I love you so so much xx"
+    },
+    erin: {
+        name: "Erin <3 (Gorgeous, astonishing lady)",
+        image: "images/erin-flower-detail.png",
+        description: "Dear the most gorgeous person I will always look up to. I appreciate you more than youll ever know. I am SO grateful you're here and in my life. You have such a beatiful soul and youre so stunning and you are so genuine. Thank you for always offering a bed if I am coming to Auckland and thank you for being you. I love you so freaking much its not funny."
+    },
+    kellie: {
+        name: "Kellie <3 (Funny, lovely lady)",
+        image: "images/kellie-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    kelsie: {
+        name: "Kelsie (Book hoarder)",
+        image: "images/kelsie-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    klara: {
+        name: "Klara-mei Li <3 (My french baguette)",
+        image: "images/klara-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    lee: {
+        name: "Lee <3 (Smart, gorgeous lady)",
+        image: "images/lee-flower-detail.png",
+        description: "Dear my unhinged sister from another mother. You are so fucking funny I literally snort because of you. Thank you for being amazing and so caring. You're so sweet and you bring such fun and amazing energy to the groupchat. I always love seeing you in the groupchat. I couldn't be more grateful to have you as a book bestie. I love you so so much xx"
+    },
+    lydia: {
+        name: "Lydia <3 (Gorgeous scat queen)",
+        image: "images/lydia-flower-detail.png",
+        description: "Dear scat queen. Thank you for always being super sweet and calling me out when its needed. You always know how to make me laugh with the random shit you say. Youre absolutely gorgeous and have the most beautiful personality. Thank you for being you, thank you for always being gorgeous. Love you xx"
+    },
+    'sarah-elizabeth': {
+        name: "Sarah-Elizabeth <3 (Witchy and sexy, GORGEOUS lady)",
+        image: "images/sarah-elizabeth-flower-detail.png",
+        description: "Dear you beautiful lady. You are so gorgeous, I am so grateful to have been in your presence when you came over, thank you for being so sweet and stunning. You make life so much brighter to anyone who is around you. Thank you for being in my life. I absolutely love your creativity and your vibes you gorgeous lovely gal. Thank you. Love you xx"
+    },
+    steph: {
+        name: "Steph <3 (Mummy xx)",
+        image: "images/stephsflower.png",
+        description: "Dear mummy, hi  you sexy lady. Thank you for being so sweet. Thank you for calling me out when its needed and not standing for shit and calling bullshit. You're so beautiful, gorgeous, stunning and radiant. I am grateful to call you my book bestie, I love that  youre so sweet and genuine. Thank you for being you, thank you for being in my life. Love you xx"
+    },
+    sydney: {
+        name: "Sydney <3 (Radiant and stunning lady)",
+        image: "images/sydney-flower-detail.png",
+        description: "Dear you gorgeous human being. Thank you for being you, thank you for being just so bloody kind, amazing and caring. You bring so much love to the groupchat, I am so glad you're in the groupchat and SO glad that I get to call you my book bestie. Seriously, thank you for being you. Love you so so SOOOOOO much xx"
+    },
+    tash: {
+        name: "Tash <3 (Talented and stunning lady)",
+        image: "images/tash-flower-detail.png",
+        description: "Dear you talented and gorgeous lady. I am so glad to call you my book bestie. Thank you for saying the most unhinged shit in the groupchats at the most random fucking times. Youre so fucking gorgeous and talented. I love how sweet you are and just how I know I can rely on you whenever I need to. I think you're gorgeous, stunning and fucking talented. Because what the fuck do you mean you can pull out yarn and just knit while we yap??? the talent, im envious. Anyway, I love you so much, thank you for being you xx"
+    },
+    taylor: {
+        name: "Taylor <3 (fucking sexy lady)",
+        image: "images/taylor-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    pashy: {
+        name: "Pashy <3 (Radiant, beauty goddess)",
+        image: "images/pashy-flower-detail.png",
+        description: "Dear you gorgeous sexy, fucking amazing lady. To know that we met without knowing it at a concert, I am so proud and happy to call you my book bestie. I am so grateful for you, knowing I can rely on you, no matter what and that you will always provide the best advice. I'm sad that you will be leaving chch end of the year but just know, that you have a comfy bed (couch) always here for you. Thank you for being you, thank you for being amazing, supportive, caring and funny. I love you so much, more than youll ever know."
+    },
+    shelb: {
+        name: "Shelb <3 (Smart, stunning and radiant lady)",
+        image: "images/shelb-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    michelle: {
+        name: "Michelle <3 (Gorgeous, sexy lady)",
+        image: "images/michelle-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED"
+    },
+    bessie: {
+        name: "Bessie <3 (Stunning, sweet gorgeous lady)",
+        image: "images/bessie-flower-detail.png",
+        description: "UNDER CONSTRUCTION UNTIL FLOWER IS RECEIVED."
+    },
+    rosebud: {
+        name: "Rosey <3 (Smart, sexy and funny lady)",
+        image: "images/rosebud-flower-detail.png",
+        description: "Dear you gorgeous, sexy lady. I'm so glad to be your friend, you are so funny as say such unhinged shit it absolutely makes me laugh and snort. You're truly amazing, caring and wonderful. I love you so much and I will always count my lucky stars knowing that I have you as a book bestie. I love you so freaking much xx"
+    }
+};
+
+// Function to show flower details
+function showFlower(flowerName) {
+    // Get the container and flower info elements
+    const container = document.getElementById('mainContainer');
+    const flowerInfo = document.getElementById('flowerInfo');
+    const flowerNameEl = document.getElementById('flowerName');
+    const flowerImageEl = document.getElementById('flowerImage');
+    const flowerDescriptionEl = document.getElementById('flowerDescription');
+    
+    // Get the flower data
+    const flower = flowerData[flowerName];
+    
+    if (flower) {
+        // Update the flower information
+        flowerNameEl.textContent = flower.name;
+        flowerImageEl.src = flower.image;
+        flowerImageEl.alt = flower.name;
+        flowerDescriptionEl.textContent = flower.description;
+        
+        // Show the flower info
+        flowerInfo.style.display = 'block';
+        
+        // Transition to sky view
+        container.classList.add('sky-view');
+    }
+}
+
+// Function to go back to garden
+function backToGarden() {
+    const container = document.getElementById('mainContainer');
+    const flowerInfo = document.getElementById('flowerInfo');
+    
+    // Hide flower info
+    flowerInfo.style.display = 'none';
+    
+    // Transition back to garden view
+    container.classList.remove('sky-view');
+}
